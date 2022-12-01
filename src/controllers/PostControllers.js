253e9b563e7c18917ea8186be5e1d62d965ad54c
valeviit <valeviit@icloud.com>
2022-12-02 00:48:33 +0100

    edit  folder structure

--- src/controllers/PostControllers.js
+++ src/controllers/PostControllers.js
@@ -1,21 +1,21 @@
 import { get } from "mongoose";
-import PostServices from "../../services/PostServices.js";
+import PostServices from "../services/PostServices.js";
 
 class PostControllers {
-    async getAll(req,res){
-        try{
+    async getAll(req, res) {
+        try {
             const posts = await PostServices.getAll();
             res.status(200).json(posts);
-        }catch(error){
+        } catch (error) {
             res.status(500).json(error)
         }
     }
 
-    async create(req,res){
-        try{
-            const post= await PostServices.create(req.body);
+    async create(req, res) {
+        try {
+            const post = await PostServices.create(req.body);
             res.status(201).json(post)
-        }catch(error){
+        } catch (error) {
             res.status(500).json(error)
         }
     }
--- src/services/PostServices.js
+++ src/services/PostServices.js
@@ -1,41 +1,41 @@
-import Post from "../src/models/Post.js";
+import Post from "../models/Post.js";
 
 
 class PostServices {
 
-   async getAll(){
-    const posts = await Post.find();
-    return posts
+   async getAll() {
+      const posts = await Post.find();
+      return posts
    }
 
-   async getOne(id){
-      if(!id){
+   async getOne(id) {
+      if (!id) {
          throw new Error("Id Missing")
       }
       const post = await Post.findById(id);
       return post
    }
 
-   async delete(id){
-      if(!id){
-         res.status(500).json({message:"id Missing!"})
+   async delete(id) {
+      if (!id) {
+         res.status(500).json({ message: "id Missing!" })
       }
       const deletedPost = await Post.findByIdAndDelete(id);
       return deletedPost
    }
 
-   async create(post){
-      const createPost = await Post.create({...post});
+   async create(post) {
+      const createPost = await Post.create({ ...post });
       return createPost
    }
 
-   async update(post){
+   async update(post) {
       //qui mongoose controlla se il post ha 1 id che mongoose ha creato
       //creando il post
-      if(!post._id){
+      if (!post._id) {
          throw new Error("Id missing!")
       }
-      const updatedPost = await Post.findByIdAndUpdate(post._id, post,{new : true} );
+      const updatedPost = await Post.findByIdAndUpdate(post._id, post, { new: true });
       return updatedPost
    }
 }
